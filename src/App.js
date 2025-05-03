710b4f6e895e75b1176559fe4668b847c1545eb3
Letícia Zalasik <leticiazalasik@gmail.com>
2025-05-03 15:46:04 -0300

    :construction: feat: Criação do esqueleto da nav bar, footer e páginas para rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 
 function App() {
   return (
     <div className="App">
-    
-        <p>
-    Costs
-        </p>
-        
+       <Router>
+  <ul>
+    <Link to="/">Home</Link>
+    <Link to="/contact">Contato</Link>
+    <Link to="/company">Empresa</Link>
+    <Link to="/newproject">Novo projeto</Link>
+
+  </ul>
+
+  <Container customClass="min-height">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/company" element={<Company />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/newproject" element={<NewProject />} />
+  </Routes>
+  </Container>
+
+  <footer>Footer</footer>
+
+</Router>
     </div>
   );
 }
