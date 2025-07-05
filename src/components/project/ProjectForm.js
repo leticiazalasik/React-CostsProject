8d4f433c5745b48d3f10a0ddf91bf384ee326706
Letícia Zalasik <leticiazalasik@gmail.com>
2025-07-05 17:36:59 -0300

    Feat: :construction: Create a fake database to be accessed by the backend and populate the form select.

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
 import styles from "./ProjectForm.module.css";
 import Input from "../Form/Input";
 import Select from "../Form/Select";
 import SubmitButton from "../Form/SubmitButton";
 
+function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
 
-function ProjectForm({btnText}) {
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -21,9 +40,13 @@ function ProjectForm({btnText}) {
         placeholder="Insira o orçamento do projeto"
       />
 
-      <Select name="category_id" text="Selecione a categoria"/>
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
 
-     <SubmitButton text={btnText}/>
+      <SubmitButton text={btnText} />
     </form>
   );
 }
